459f071937a0fe89da0126e79d10f54509f1b926
lawrance-ai <lawrenceandrewselvam_bai28@mepcoeng.ac.in>
2026-09-22 15:48:15 +0530

    Time: 0 ms (100%), Space: 12.2 MB (76.35%) - LeetHub

---
+++ 0018-4sum/0018-4sum.c
@@ -0,0 +1,67 @@
+int compare(const void* a, const void* b) {
+    int arg1 = *(const int*)a;
+    int arg2 = *(const int*)b;
+    if (arg1 < arg2) return -1;
+    if (arg1 > arg2) return 1;
+    return 0;
+}
+
+int** fourSum(int* nums, int numsSize, int target, int* returnSize, int** returnColumnSizes) {
+    *returnSize = 0;
+    if (numsSize < 4) return NULL;
+
+    qsort(nums, numsSize, sizeof(int), compare);
+
+    int capacity = 16;
+    int** result = (int**)malloc(capacity * sizeof(int*));
+    *returnColumnSizes = (int*)malloc(capacity * sizeof(int));
+
+    for (int i = 0; i < numsSize - 3; i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+        
+        if ((long long)nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) break;
+        if ((long long)nums[i] + nums[numsSize - 3] + nums[numsSize - 2] + nums[numsSize - 1] < target) continue;
+
+        for (int j = i + 1; j < numsSize - 2; j++) {
+            if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+            
+            if ((long long)nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) break;
+            if ((long long)nums[i] + nums[j] + nums[numsSize - 2] + nums[numsSize - 1] < target) continue;
+
+            int left = j + 1;
+            int right = numsSize - 1;
+
+            while (left < right) {
+                long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+
+                if (sum < target) {
+                    left++;
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    if (*returnSize >= capacity) {
+                        capacity *= 2;
+                        result = (int**)realloc(result, capacity * sizeof(int*));
+                        *returnColumnSizes = (int*)realloc(*returnColumnSizes, capacity * sizeof(int));
+                    }
+
+                    result[*returnSize] = (int*)malloc(4 * sizeof(int));
+                    result[*returnSize][0] = nums[i];
+                    result[*returnSize][1] = nums[j];
+                    result[*returnSize][2] = nums[left];
+                    result[*returnSize][3] = nums[right];
+                    (*returnColumnSizes)[*returnSize] = 4;
+                    (*returnSize)++;
+
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                }
+            }
+        }
+    }
+
+    return result;
+}
